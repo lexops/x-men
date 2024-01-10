ea5c455a07c411d2a19a04cdc5d4085b8ff7dcca
Alex Silva <134613820+lexops@users.noreply.github.com>
2024-01-10 18:17:29 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach( (personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top:0, behavior:'smooth'})
+        }
+
+        removerSelecaoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemGrandePersonagem(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
+    })
+})
+
+function removerSelecaoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
+function alterarImagemGrandePersonagem(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `imagens/card-${idPersonagem}.png`;
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
